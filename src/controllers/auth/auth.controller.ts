c2b5489d952709ca52c442a460035f8c743ecc82
Rinel <rineliniguezsosa@gmail.com>
2026-04-09 16:07:49 -0600

    [FEAT] Estableciendo una respuesta en formato json para comprobar el correcto funcionamiento

--- src/controllers/auth/auth.controller.ts
+++ src/controllers/auth/auth.controller.ts
@@ -1,5 +1,8 @@
 import { Request, Response } from 'express';
 
 export const createUser = async(req: Request, resp:Response) =>{
-    
+    resp.json({
+        ok:true,
+        msg:'registro'
+    })
 }
