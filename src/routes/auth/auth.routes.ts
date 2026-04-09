590f9e8d3d8fda284aa2e5140af3a8c45dfcd85e
Rinel <rineliniguezsosa@gmail.com>
2026-04-09 16:18:36 -0600

    [FEAT] Importando los métodos y estableciendolos como middleware en la ruta base y en la ruta renew

--- src/routes/auth/auth.routes.ts
+++ src/routes/auth/auth.routes.ts
@@ -1,17 +1,9 @@
 import { Router } from 'express';
 import { Request, Response } from 'express';
-import { createUser } from '../../controllers';
+import { checkToken, createUser, loginUser } from '../../controllers';
 
 export const authRoutes = Router();
 
 authRoutes.get('/new',createUser)
-authRoutes.post('/',(req:Request,resp:Response) =>{
-    resp.json({
-        ok:true
-    })
-})
-authRoutes.post('/renew',(req:Request,resp:Response) =>{
-    resp.json({
-        ok:true
-    })
-})
+authRoutes.post('/',loginUser)
+authRoutes.post('/renew',checkToken)
