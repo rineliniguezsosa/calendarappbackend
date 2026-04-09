79a833cffb38c2de11be8f504cafe6072e275753
Rinel <rineliniguezsosa@gmail.com>
2026-04-09 16:14:13 -0600

    [FEAT] Creando los métodos loginUser y checkToken

--- src/controllers/auth/auth.controller.ts
+++ src/controllers/auth/auth.controller.ts
@@ -6,3 +6,15 @@ export const createUser = async(req: Request, resp:Response) =>{
         msg:'registro'
     })
 }
+export const loginUser = async(req: Request, resp:Response) =>{
+    resp.json({
+        ok:true,
+        msg:'login'
+    })
+}
+export const checkToken = async(req: Request, resp:Response) =>{
+    resp.json({
+        ok:true,
+        msg:'checkToken'
+    })
+}
