0446f5e05eb3d0ccdece1f19c879fe234947f201
Rinel <rineliniguezsosa@gmail.com>
2026-04-09 14:13:42 -0600

    [FEAT] Estableciendo la ruta principal api/auth en nuestra aplicación para leer la ruta authRoutes

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import { authRoutes } from './routes';
 import dotenv from 'dotenv';
 
 dotenv.config()
 
 const app = express();
+
+app.use('/api/auth',authRoutes)
 
 app.listen(process.env.PORT,()=>{
     console.log(`App running in http://localhost:${process.env.PORT}`); 
