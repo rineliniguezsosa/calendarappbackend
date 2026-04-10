9adaa13be047bdb5f331f92dc0586ead9cc33022
Rinel <rineliniguezsosa@gmail.com>
2026-04-10 16:58:32 -0600

    [FEAT] Estableciendo las validaciones para el email y el password

--- src/validators/validators.ts
+++ src/validators/validators.ts
@@ -19,3 +19,16 @@ export const createUserValidation = [
          .isLength({ min: 8 }).withMessage('El campo password debe tener al menos 8 caracteres'),
     validaterrors
 ]
+
+export const loginValidation = [
+    body('email')
+         .trim()
+         .toLowerCase()
+         .notEmpty().withMessage('El campo email es requerido !!')
+         .isEmail().withMessage('El campo debe de estar en un formato valido de email'),
+    body('password')
+         .trim()
+         .notEmpty().withMessage('El campo password es requerido !!')
+         .isLength({ min: 8 }).withMessage('El campo password debe tener al menos 8 caracteres'),
+    validaterrors
+]
