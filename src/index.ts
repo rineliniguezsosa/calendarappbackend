47190dab42ab07a46930b558ea3a90f973eac599
Rinel <rineliniguezsosa@gmail.com>
2026-04-10 16:18:37 -0600

    [FEAT] Cambiando el orden para definir las entradas en formato json

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,8 @@ dotenv.config()
 
 const app = express();
 
-app.use('/api/auth',authRoutes)
 app.use(express.json());
+app.use('/api/auth',authRoutes)
 
 app.listen(process.env.PORT,()=>{
     console.log(`App running in http://localhost:${process.env.PORT}`); 
