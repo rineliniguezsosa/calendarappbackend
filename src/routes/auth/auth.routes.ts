1117f9659669d11c5f18bbcd088a3dc972327f5e
Rinel <rineliniguezsosa@gmail.com>
2026-04-16 16:25:18 -0600

    [FEAT] Establecemos como middleware el método validarJwt

--- src/routes/auth/auth.routes.ts
+++ src/routes/auth/auth.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import { checkToken, createUser, loginUser } from '../../controllers';
 import { createUserValidation,loginValidation } from '../../validators';
+import { validarJwt } from '../../middleware';
 
 export const authRoutes = Router();
 
 authRoutes.post('/new',createUserValidation,createUser)
 authRoutes.post('/',loginValidation,loginUser)
-authRoutes.post('/renew',checkToken)
+authRoutes.post('/renew',validarJwt,checkToken)
