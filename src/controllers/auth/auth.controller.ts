5fd5566f1fed64043dc18256edb4bcebe0efaf2a
Rinel <rineliniguezsosa@gmail.com>
2026-04-15 22:17:30 -0600

    [FEAT] Estableciendo el bloque try catch para comprobar el bloque de la creación del usario
    - estableciendo un status 500 el cual es un error del servidor donde establecemos el mensaje al usuario

--- src/controllers/auth/auth.controller.ts
+++ src/controllers/auth/auth.controller.ts
@@ -3,14 +3,24 @@ import { usuarioModel } from '../../models';
 
 export const createUser = async(req: Request, resp:Response) =>{
     // const {name,email,password} = req.body;
-    const usuario = new usuarioModel(req.body);
-
-    await usuario.save();
-
-    resp.status(201).json({
-        ok:true,
-        msg:'registro',
-    })
+    try {
+        
+        const usuario = new usuarioModel(req.body);
+    
+        await usuario.save();
+    
+        resp.status(201).json({
+            ok:true,
+            msg:'registro',
+        })
+    } catch (error) {
+        console.log('error in createUser: ',error);
+        resp.status(500).json({
+            ok:true,
+            msg:'Porfavor hable con el administrador',
+        })
+        
+    }
 }
 export const loginUser = async(req: Request, resp:Response) =>{
     const {email,password} = req.body;
