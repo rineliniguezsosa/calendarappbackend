83b2a834fa35dc33b26c6ccdcfb0cf2f1047d313
Rinel <rineliniguezsosa@gmail.com>
2026-04-15 19:10:51 -0600

    [FEAT] Cambios en el método dbConnection
    - eliminando logs
    - estableciendo el titulo de error en el log

--- src/config/database/config.ts
+++ src/config/database/config.ts
@@ -1,16 +1,17 @@
 import mongoose from "mongoose";
 
-export const dbConection = async() =>{
+export const dbConnection = async() =>{
     try {
         const dbUrl = process.env.DB_URL;
-        console.log(dbUrl);
         
         if (!dbUrl) {
             throw new Error('DB_URL environment variable is not defined');
         }
-        await mongoose.connect(dbUrl)
+        await mongoose.connect(dbUrl);
+        console.log('coneccted!!');
+        
     } catch (error) {
-        console.log(error);
+        console.log("error: ",error);
         throw new Error('Error al inicializar la DB')
     }
 }
