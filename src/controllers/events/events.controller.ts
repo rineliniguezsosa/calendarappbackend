9f188442fd5918df1e77f544801509cd727dc393
Rinel <rineliniguezsosa@gmail.com>
2026-04-17 12:04:15 -0600

    [FEAT] Creando el método deleteEvents

--- src/controllers/events/events.controller.ts
+++ src/controllers/events/events.controller.ts
@@ -45,3 +45,18 @@ export const updateEvents = async(req:Request,resp:Response) =>{
         })
     }
 }
+export const deleteEvents = async(req:Request,resp:Response) =>{
+    try {
+        resp.status(201).json({
+            ok:true,
+            msg:''
+        })
+    } catch (error) {
+        console.log(error);
+        
+        resp.status(400).json({
+            ok:false,
+            msg:'error'
+        })
+    }
+}
