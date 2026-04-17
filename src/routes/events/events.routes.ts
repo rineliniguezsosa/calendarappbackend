dee1a879722821e412229db94e0bf4003f0e8b4f
Rinel <rineliniguezsosa@gmail.com>
2026-04-17 12:07:49 -0600

    [FEAT] Creando multiples solciitudes http para crear, actualizar y eliminar
    - estableciendo parametros id
    - importando los métodos

--- src/routes/events/events.routes.ts
+++ src/routes/events/events.routes.ts
@@ -1,6 +1,9 @@
 import { Router } from 'express'
-import { getEvents } from '../../controllers';
+import { getEvents,createEvents,updateEvents,deleteEvents } from '../../controllers';
 
 export const eventsRouter = Router();
 
 eventsRouter.get('/',getEvents)
+eventsRouter.post('/',createEvents)
+eventsRouter.put('/:id',updateEvents)
+eventsRouter.delete('/:id',deleteEvents)
