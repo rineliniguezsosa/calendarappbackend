60caac571ef7ec34d71171dd5c9a5b78bfbe85f9
Rinel <rineliniguezsosa@gmail.com>
2026-04-17 12:03:07 -0600

    [FEAT] Creando el método createEvents

--- src/controllers/events/events.controller.ts
+++ src/controllers/events/events.controller.ts
@@ -15,3 +15,18 @@ export const getEvents = async(req:Request,resp:Response) =>{
         })
     }
 }
+export const createEvents = async(req:Request,resp:Response) =>{
+    try {
+        resp.status(201).json({
+            ok:true,
+            msg:''
+        })
+    } catch (error) {
+        console.log(error);
+        
+        resp.status(400).json({
+            ok:false,
+            msg:'error'
+        })
+    }
+}
