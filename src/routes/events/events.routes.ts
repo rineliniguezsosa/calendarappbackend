79d65482368958b6a24ef6049c9eb5307a06a30c
Rinel <rineliniguezsosa@gmail.com>
2026-04-21 13:25:50 -0600

    [FEAT] Cambios en las rutas principal para crear los eventos de calendario
    - estableciendo como middleware createEventValidator

--- src/routes/events/events.routes.ts
+++ src/routes/events/events.routes.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express'
 import { getEvents,createEvents,updateEvents,deleteEvents } from '../../controllers';
 import { validarJwt } from '../../middleware';
+import { createEventValidator } from '../../validators';
 
 export const eventsRouter = Router();
 
 eventsRouter.use(validarJwt)
 
 eventsRouter.get('/',getEvents)
-eventsRouter.post('/',createEvents)
+eventsRouter.post('/',createEventValidator,createEvents)
 eventsRouter.put('/:id',updateEvents)
 eventsRouter.delete('/:id',deleteEvents)
